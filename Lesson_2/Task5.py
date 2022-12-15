f101634922797afa5535802767a1203dde2b78a2
acid-harry <igorkrivouhov@gmail.com>
2022-12-15 17:42:14 +0300

    Task 5 is done

--- Lesson_2/Task5.py
+++ Lesson_2/Task5.py
@@ -1,10 +1,10 @@
 #Реализуйте алгоритм нахождения(генерации) рандомного(случайного) числа.(Не используя библиотеки связанные с рандомом)
 
-the_set = set()
+num = set()
 
 for i in range(100000):
-    the_set.add(str(i))
+    num.add(str(i))
 
-for e in the_set:
+for e in num:
     print(int(e))
     break
